94558c2cef0916834a34dd95f8ca1656767294df
Bunyamin-1058754 <1058754@hr.nl>
2024-04-08 16:41:39 +0200

    logos toegevoegd, fetch bijgewerkt werkt half, styling react

--- react-weather-app/src/App.js
+++ react-weather-app/src/App.js
@@ -1,16 +1,18 @@
 import './App.css';
 import Search from './components/search/search';
+import CurrentWeather from './components/search/current-weather/current-weather';
 
 function App() {
 
 
   const handleOnSearchChange = (searchData) => {
-    console.log(searchData);
+    
   }
   
   return (
     <div className="container">
     <Search onSearchChange={handleOnSearchChange}/> 
+    <CurrentWeather/>
     </div>
   );
 }

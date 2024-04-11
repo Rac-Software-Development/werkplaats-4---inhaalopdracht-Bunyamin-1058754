6511078720b517df08641bd0b8e73be50b52df01
Bunyamin-1058754 <1058754@hr.nl>
2024-04-11 22:31:15 +0200

    forecast.js afgemaakt + styling forecast veranderingen in api

--- react-weather-app/src/api.js
+++ react-weather-app/src/api.js
@@ -1,4 +1,4 @@
-const url = 'https://wft-geo-db.p.rapidapi.com/v1/geo/cities';
+const url = 'https://wft-geo-db.p.rapidapi.com/v1/geo/cities?maxPopulation=500000';
 
 export const geoApiOptions = {
 	method: 'GET',
